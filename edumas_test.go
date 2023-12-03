ac6d50a07c02d5d403440c650766f69672265eed
miqbalramadhan18 <miqbalr18@gmail.com>
2023-12-03 22:03:56 +0700

    update test.go

--- edumas_test.go
+++ edumas_test.go
@@ -18,6 +18,15 @@ func TestCreateNewUserRole(t *testing.T) {
 	CreateNewUserRole(mconn, "user", userdata)
 }
 
+func TestCreateNewAdminRole(t *testing.T) {
+	var admindata Admin
+	admindata.Username = "edumasmin"
+	admindata.Password = "edumasmin1"
+	admindata.Role = "admin"
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+	CreateNewAdminRole(mconn, "admin", admindata)
+}	
+
 func TestDeleteUser(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "edumasapk")
 	var userdata User
@@ -42,6 +51,23 @@ func CreateNewUserToken(t *testing.T) {
 	}
 }
 
+func CreateNewAdminToken(t *testing.T) {
+	var admindata Admin
+	admindata.Username = "admin"
+	admindata.Password = "mantap"
+	admindata.Role = "admin"
+
+	// Create a MongoDB connection
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+
+	// Call the function to create a admin and generate a token
+	err := CreateAdminAndAddToken("your_private_key_env", mconn, "admin", admindata)
+
+	if err != nil {
+		t.Errorf("Error creating admin and token: %v", err)
+	}
+}
+
 func TestGFCPostHandlerUser(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "edumasapk")
 	var userdata User
@@ -49,6 +75,15 @@ func TestGFCPostHandlerUser(t *testing.T) {
 	userdata.Password = "mantap"
 	userdata.Role = "user"
 	CreateNewUserRole(mconn, "user", userdata)
+}
+
+func TestGFCPostHandlerAdmin(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+	var admindata Admin
+	admindata.Username = "edumas123"
+	admindata.Password = "mantap"
+	admindata.Role = "Admin"
+	CreateNewAdminRole(mconn, "admin", admindata)
 }
 
 func TestReport(t *testing.T) {
@@ -98,7 +133,21 @@ func TestHashFunction(t *testing.T) {
 	fmt.Println("Hash Password : ", hash)
 	match := CompareHashPass(userdata.Password, res.Password)
 	fmt.Println("Match:   ", match)
+}
 
+func TestHashFunctionAdmin(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+	var admindata Admin
+	admindata.Username = "tes123"
+	admindata.Password = "tes321"
+
+	filter := bson.M{"username": admindata.Username}
+	res := atdb.GetOneDoc[Admin](mconn, "admin", filter)
+	fmt.Println("Mongo Admin Result: ", res)
+	hash, _ := HashPass(admindata.Password)
+	fmt.Println("Hash Password : ", hash)
+	match := CompareHashPass(admindata.Password, res.Password)
+	fmt.Println("Match:   ", match)
 }
 
 func TestIsPasswordValid(t *testing.T) {
@@ -111,6 +160,16 @@ func TestIsPasswordValid(t *testing.T) {
 	fmt.Println(anu)
 }
 
+func TestIsPasswordValidAdmin(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+	var admindata Admin
+	admindata.Username = "ganteng"
+	admindata.Password = "gelis"
+
+	anu := IsPasswordValidAdmin(mconn, "admin", admindata)
+	fmt.Println(anu)
+}
+
 func TestUserFix(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "edumasapk")
 	var userdata User
@@ -120,6 +179,15 @@ func TestUserFix(t *testing.T) {
 	CreateUser(mconn, "user", userdata)
 }
 
+func TestAdminFix(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "edumasapk")
+	var admindata Admin
+	admindata.Username = "edumas"
+	admindata.Password = "edumas0"
+	admindata.Role = "admin"
+	CreateAdmin(mconn, "admin", admindata)
+}
+
 func TestLoginn(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "pasabarapk")
 	var userdata User
@@ -127,4 +195,13 @@ func TestLoginn(t *testing.T) {
 	userdata.Password = "edumas0"
 	IsPasswordValid(mconn, "user", userdata)
 	fmt.Println(userdata)
+}
+
+func TestLoginnAdmin(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "pasabarapk")
+	var admindata Admin
+	admindata.Username = "edumas"
+	admindata.Password = "edumas0"
+	IsPasswordValidAdmin(mconn, "admin", admindata)
+	fmt.Println(admindata)
 }
